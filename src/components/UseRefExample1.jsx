e0ce1a130024d0803b1ad9f15da9ae8453628498
razvanr91 <razvan.roman91@outlook.com>
2022-03-13 20:05:00 +0000

    Create user ref example

---
+++ src/components/UseRefExample1.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react'
+
+function UseRefExample1() {
+    const inputRef = useRef();
+    const paraRef = useRef();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputRef.current.value)
+        inputRef.current.value = "Hello"
+        paraRef.current.innerText = "Goodbye!!";
+    }
+  return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <label htmlFor="name">Name</label>
+            <input type="text" ref={inputRef} className='form-control mb-2' />
+            <button type='submit' className='btn btn-primary'>Submit</button>
+        </form>
+        <p onClick={() => inputRef.current.focus()} ref={paraRef}>
+            Hello
+        </p>
+    </div>
+  )
+}
+
+export default UseRefExample1
